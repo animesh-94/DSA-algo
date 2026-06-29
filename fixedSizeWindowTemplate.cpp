aab2f0256bf3e7dc388e8917fa52a5ebbd63a23c
Animesh Yadav <154960710+animesh-94@users.noreply.github.com>
2026-06-30 01:39:33 +0530

    Create fixedSizeWindowTemplate.cpp

---
+++ fixedSizeWindowTemplate.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    
+    deque<int> dq;
+    
+    for(int i=0; i<n; i++){
+        while(!dq.empty() && dq.front()<=i-k){
+            dq.pop_front();
+        }
+        
+        while(!dq.empty() && a[dq.back()] > a[i]){
+            dq.pop_back();
+        }
+        
+        dq.push_back(i);
+        
+        if(i>=k-1){
+            cout << a[dq.front()] << " ";
+        }
+    }
+}
+
+int main(){
+    int t=1;
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
